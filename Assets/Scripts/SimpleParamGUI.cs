4cb6603a8f3b2962c44c497dba0e1fe398695506
fdurupinar <f.durupinar@gmail.com>
2021-01-25 16:13:17 -0500

    cleaned up

--- Assets/Scripts/SimpleParamGUI.cs
+++ Assets/Scripts/SimpleParamGUI.cs
@@ -1,52 +1,24 @@
 ﻿
-using System;
-using System.Linq;
+
 using UnityEngine;
-using System.IO;
-using UnityEngine.SceneManagement;
-using System.Collections;
-using System.Collections.Generic;
 
 /// <summary>
-/// GUI functions to tune character personality
+/// GUI functions to run selected animation
 /// </summary>
 public class SimpleParamGUI : GUIController {
-    RaycastHit _hit;
 
-    int _agentSelInd;
-
-
-
-    private float[] _personality = new float[5]; //-1 0 1 
-    private string[] _personalityName = { "O", "C", "E", "A", "N" };
-
-    private int _persMin = -1;
-    private int _persMax = 1;
-
-    private DriveParams[] _driveParams = new DriveParams[32];
-
-    private PersonalityMapper _persMapper;
-
-    private AnimationInfo[] _agentScripts;
-
-
-    private DropDownRect _dropDownRectAnimNames;
-    private DropDownRect _dropDownRectAgents;
 
     
+    private DriveParams[] _driveParams = new DriveParams[32];
 
+    
+    private AnimationInfo[] _agentScripts;
 
-    private bool _lockHand = false;
-
-
+    
     private void Start() {
-
 
         _agentScripts = transform.GetComponentsInChildren<AnimationInfo>();
 
-
-        _dropDownRectAgents = new DropDownRect(new Rect(115, 20, 90, 300));
-        _dropDownRectAnimNames = new DropDownRect(new Rect(210, 20, 90, 300));
 
         for(int i = 0; i < 32; i++) {
             _driveParams[i] = new DriveParams();
@@ -58,15 +30,10 @@ public class SimpleParamGUI : GUIController {
             _driveParams[i].ReadValuesDrives(i);
 
 
-
-        _agentSelInd = 0;
-
-
         foreach(AnimationInfo t in _agentScripts)
             Reset(t);
 
 
-        MathDefs.SetSeed(30);
 
     }
 
@@ -79,11 +46,8 @@ public class SimpleParamGUI : GUIController {
 
         GUILayout.BeginArea(new Rect(5, 20, 205, Screen.height));
 
-  
-
         if(GUILayout.Button("Play"))
             PlayAgents();
-
 
         
         GUILayout.EndArea();
@@ -105,14 +69,6 @@ public class SimpleParamGUI : GUIController {
         
     }
 
-    void StopAgents() {
-        foreach(AnimationInfo t in _agentScripts) {
-            StopAnimations(t);
-
-        }
-    }
-
-    
 
 }
 
